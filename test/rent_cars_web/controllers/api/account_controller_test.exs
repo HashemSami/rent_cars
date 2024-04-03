fc638ae6170d6024fc885cff40d1861c34b7865b
Hashem Sami <hashem.sf@gmail.com>
2024-04-03 21:08:30 +0300

    adding api test for accounts

--- test/rent_cars_web/controllers/api/account_controller_test.exs
+++ test/rent_cars_web/controllers/api/account_controller_test.exs
@@ -2,25 +2,15 @@ defmodule RentCarsWeb.Api.AccountControllerTest do
   use RentCars.ConnCase
 
   alias RentCars.Accounts
+  import RentCars.AccountsFixtures
 
   describe "create users" do
-    setup do
-      user_attrs = %{
-        first_name: "Hash first_name",
-        last_name: "Hash last_name",
-        user_name: "Hash user_name",
-        password: "Hash password",
-        password_confirmation: "Hash password",
-        password_hash: "Hash password_hash",
-        email: "Email@hash",
-        drive_license: "Hash drive_license"
-      }
 
-      %{user_attrs: user_attrs}
-    end
 
-    test "create_user/1 with valid dates", %{user_attrs: user_attrs} do
-      valid_attrs = user_attrs
+    test "create user when the data is valid", %{user_attrs: user_attrs} do
+      valid_attrs = user_attrs()
+
+      conn = post(conn, ~p"/api/")
 
       assert {:ok, user} = Accounts.create_user(valid_attrs)
       assert user.first_name == valid_attrs.first_name
